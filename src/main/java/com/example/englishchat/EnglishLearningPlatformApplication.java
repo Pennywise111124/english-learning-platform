1325ca2f9b8cc6b42ef3a49891790cef0aa4f93c
Danaai <115464649+Danaai@users.noreply.github.com>
2026-08-29 10:44:31 +0700

    Initial commit from Spring Initializr

---
+++ src/main/java/com/example/englishchat/EnglishLearningPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.englishchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishLearningPlatformApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+	}
+
+}
---
+++ src/test/java/com/example/englishchat/EnglishLearningPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.englishchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishLearningPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
